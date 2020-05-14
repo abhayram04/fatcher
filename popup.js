c12819cfe569685fd753cc64f0fa7115a534b24a
Abhay Ram <abhayram04@gmail.com>
2020-05-14 14:46:04 +0530

    Visual changes
    Added title. UI still needs work.

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 var turl = "";
 
 function reportHandler(e) {
-	e.preventDefault();
+	
 	// Clicked report button
 	fetch("https://fatcher-back.herokuapp.com/report", {
 		method: "POST",
@@ -9,10 +9,12 @@ function reportHandler(e) {
 			"content-type": "application/json",
 		},
 		body: JSON.stringify({
-			url: turl,
+			url: turl
 		}),
 	})
+		
 		.then((res) => res.json())
+		
 		.then((response) => {
 			console.log(response);
 			updateReportCount();
@@ -43,12 +45,15 @@ function updateReportCount() {
 
 document.addEventListener("DOMContentLoaded", function () {
 	var query = { active: true, currentWindow: true };
+	
 	chrome.tabs.query(query, (tabs) => {
 		var currentTab = tabs[0]; // there will be only one in this array
 		turl = currentTab.url;
+		tit = currentTab.title;
+		document.querySelector("#ctit").innerHTML = tit;
 		console.log(turl); // also has properties like currentTab.id
 		document.querySelector("#url").innerHTML = turl;
-
+		
 		document
 			.querySelector("#reportPage")
 			.addEventListener("click", reportHandler);
